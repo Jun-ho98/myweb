64b9735bbff191de9e4d0314151d3c1784ef65fa
DESKTOP-0941RUJ\user1 <soooh1030@naver.com>
2026-01-16 11:49:00 +0900

    회원가입페이지 유효성 검증

--- app/index.js
+++ app/index.js
@@ -1,1 +1,30 @@
-console.log("hellow");
+function handleJoin() {
+    const userId = document.querySelector("input[name=userId").value;
+    const userPw = document.querySelector("input[name=userPw").value;
+    const userPw2 = document.querySelector("input[name=userPw2").value;
+    const userNick = document.querySelector("input[name=userNick").value;
+
+    //id 검증 (영어 및 숫자 허용 , 4~8글자)
+    ;
+    const idOk = /^[a-z0-9]{4,8}$/.test(userId);
+    if (!idOk) {
+        alert("아이디 확인 必")
+        return false;
+    };
+
+    //pw 검증
+    const pwOk1 = /^[0-9]{4,8}$/.test(userPw);
+    const pwOk2 = userPw === userPw2;
+    const pwOk = pwOk1 && pwOk2;
+    if (!pwOk) {
+        alert("비밀번호 확인 必");
+        return false;
+    };
+
+    //nick 검증
+    const nickOk = /^[가-힣]{2,4}$/.test(userNick);
+    if (!nickOk) {
+        alert("닉네임 확인 必");
+        return false;
+    }
+}
